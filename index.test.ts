a432435b5fa7cf8f85b0526a26099cac1ce23633
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-06-18 20:21:51 +0200

    feat: matrix.has

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,48 @@
+import { describe } from "vitest";
 import { it, expect } from "vitest";
-import { add } from "./index";
+import { Matrix } from "./index";
 
-it("works", () => {
-  expect(add(1, 3)).toBe(4);
+describe("matrix", () => {
+  const exampleMatrix = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+  ];
+
+  describe("has", () => {
+    it("works for (0, 0)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(0, 0)).toBe(true);
+    });
+
+    it("works for (0, last)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(0, 2)).toBe(true);
+    });
+
+    it("fails for (0, last + 1)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(0, 3)).toBe(false);
+    });
+
+    it("works for (last, last)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(2, 2)).toBe(true);
+    });
+
+    it("fails for (last + 1, last + 1)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(3, 3)).toBe(false);
+    });
+
+    it("works for (last, 0)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(2, 0)).toBe(true);
+    });
+
+    it("fails for (last + 1, 0)", () => {
+      const m = new Matrix(exampleMatrix);
+      expect(m.has(3, 0)).toBe(false);
+    });
+  });
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,34 @@
-export function add(a: number, b: number) {
-  return a + b;
+export class Matrix {
+  #matrix: string[][];
+
+  constructor(matrix: string[][]) {
+    this.#matrix = matrix;
+  }
+
+  has(x: number, y: number) {
+    if (this.#matrix.length <= y) return false;
+    if (this.#matrix[y].length <= x) return false;
+    return true;
+  }
+
+  get(x: number, y: number): string | false {
+    if (this.has(x, y)) {
+      return this.#matrix[y][x];
+    }
+    return false;
+  }
+}
+
+class WordSearch {
+  // matrix of chars
+  #words: string[][];
+
+  constructor(matrix: string[]) {
+    this.#words = matrix.map((row) => row.split(""));
+  }
+
+  find(words: string[]) {
+    for (const word of words) {
+    }
+  }
 }
